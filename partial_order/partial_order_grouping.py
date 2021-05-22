2b662c39aa306bbf9768afe505dac1e9dbfc6e72
avinash737 <avinashgopal737@gmail.com>
2021-05-22 19:56:37 +0200

    Updated partial_order_grouping.py
    Added the method get_partial_order_dataframe() that returns the list of data frames containing all information on all partial orders with a new attribute 'concept:index' that stores sequencing information for each case.
    Added the method get_partial_order_groups() that uses the return data structure from get_partial_order_dataframe() and then returns the list of data frames and lists that stores the information on each group and the corresponding case ID's in the list of the same index..

--- partial_order/partial_order_grouping.py
+++ partial_order/partial_order_grouping.py
@@ -5,54 +5,111 @@ from pm4py.util.constants import CASE_CONCEPT_NAME
 from partial_order_detection import get_partial_orders_from_selected_file as get_partial_orders
 
 
-def get_partial_order_groups():
+def get_partial_order_dataframes():
     partial_order_log = get_partial_orders()
     df = log_converter.apply(partial_order_log, variant=log_converter.Variants.TO_DATA_FRAME)
-
     cases = df[CASE_CONCEPT_NAME].unique()
-    """
-    partial_order_groups_init is a 3 dimensional array that initially stores all partial orders in th following format:
-    partial_order_groups_init[group_number][index_for_each_timestamp][index_for_events_with_the_same_timestamp]
-    """
-    partial_order_groups_init = [[0] * 10] * 10
-    i = 0
-    j = 0
+
+    partial_order_dataframes = list()
+
+    i = -1
 
     for case_id in cases:
-        case = df.loc[df[CASE_CONCEPT_NAME] == case_id]
-        # print(case)
-        event_data = {'name': [str(case.iloc[0][0])], 'transition': [str(case.iloc[0][1])],
-                      'timestamp': [pd.to_datetime(case.iloc[0][2])],
-                      'case': [int(case.iloc[0][3])]}
-        partial_order_groups_init[i][j] = pd.DataFrame(event_data)
-        for event in range(1, len(case)):
-            if case.iloc[event][2] == case.iloc[event - 1][2]:
-                event_data = {'name': str(case.iloc[event][0]), 'transition': str(case.iloc[event][1]),
-                              'timestamp': pd.to_datetime(case.iloc[event][2]),
-                              'case': int(case.iloc[event][3])}
-                partial_order_groups_init[i][j] = partial_order_groups_init[i][j].append(event_data,
-                                                                                         ignore_index=True)
-            else:
-                j += 1
-                event_data = {'name': [str(case.iloc[event][0])], 'transition': [str(case.iloc[event][1])],
-                              'timestamp': [pd.to_datetime(case.iloc[event][2])],
-                              'case': [int(case.iloc[event][3])]}
-                partial_order_groups_init[i][j] = pd.DataFrame(event_data)
-
-        print(partial_order_groups_init[i])
         i += 1
-        j = 0
+
+        case = df.loc[df[CASE_CONCEPT_NAME] == case_id]
+
+        ind_list = case.index.tolist()
+        ind = 0
+
+        ind_list[0] = ind
+
+        partial_order_dataframes.append([])
+
+        partial_order_dataframes[i] = pd.DataFrame()
+        partial_order_dataframes[i] = partial_order_dataframes[i].append(case.iloc[0], ignore_index=True)
+
+        for event in range(1, len(case)):
+            if case.iloc[event - 1][2] == case.iloc[event][2]:
+                partial_order_dataframes[i] = partial_order_dataframes[i].append(case.iloc[event], ignore_index=True)
+            else:
+                ind += 1
+                partial_order_dataframes[i] = partial_order_dataframes[i].append(case.iloc[event],
+                                                                                 ignore_index=True)
+            ind_list[event] = ind
+
+        partial_order_dataframes[i].index = ind_list
+        partial_order_dataframes[i].reset_index(inplace=True)
+        partial_order_dataframes[i] = partial_order_dataframes[i].rename(columns={'index': 'concept:index'})
+        partial_order_dataframes[i].astype({'concept:index': 'int64'}).dtypes
+
+        for i in range(0, len(partial_order_dataframes)):
+            partial_order_dataframes[i] = partial_order_dataframes[i].sort_values(['concept:index'], ascending=True) \
+                .groupby(['concept:index'], sort=False) \
+                .apply(lambda x: x.sort_values(['concept:name'], ascending=True)) \
+                .reset_index(drop=True)
+
+    return partial_order_dataframes
 
 
-def test_dataframe():
-    a = {'Car': ['a', 'b', 'c'], 'Type': ['big', 'smol', 'tiny']}
-    df_a = pd.DataFrame(a)
-    print(df_a)
-    b = {'Car': 'd', 'Type': 'boogula'}
-    df_a = df_a.append(b, ignore_index=True)
-    print(df_a)
+def get_partial_order_groups():
+    partial_order_dataframes = get_partial_order_dataframes()
+
+    order_attributes = ['concept:index', 'concept:name', 'lifecycle:transition', 'case:concept:name']
+    group_attributes = ['concept:index', 'concept:name', 'lifecycle:transition']
+
+    partial_orders = list()
+
+    for i in range(0, len(partial_order_dataframes)):
+        partial_orders.append([])
+        partial_orders[i] = partial_order_dataframes[i][order_attributes]
+
+    partial_order_groups = list()
+
+    g = 0
+
+    while len(partial_orders):
+
+        partial_order_groups.append([])
+        partial_order_groups[g].append([])
+        partial_order_groups[g][0] = pd.DataFrame()
+        partial_order_groups[g][0] = partial_orders[0][group_attributes]
+        partial_order_groups[g][0] = partial_order_groups[g][0].sort_values(['concept:index'], ascending=True) \
+            .groupby(['concept:index'], sort=False) \
+            .apply(lambda x: x.sort_values(['concept:name'], ascending=True)) \
+            .reset_index(drop=True)
+
+        partial_order_groups[g].append([])
+        partial_order_groups[g][1] = pd.DataFrame(columns=['case:concept:name'])
+        val = pd.Series([partial_orders[0]['case:concept:name'][0]], dtype='str')
+        partial_order_groups[g][1]['case:concept:name'] = \
+            partial_order_groups[g][1]['case:concept:name'].append(val, ignore_index=True)
+
+        partial_orders.remove(partial_orders[0])
+
+        for case in range(0, len(partial_orders)):
+            if partial_order_groups[g][0].equals(partial_orders[case][group_attributes]):
+                if partial_orders[case]['case:concept:name'][0] not in \
+                        partial_order_groups[g][0]['case:concept:name']:
+                    partial_order_groups[g][1]['case:concept:name'].append(
+                        [partial_orders[case]['case:concept:name'][0]], ignore_index=True)
+                partial_orders.remove(partial_orders[case])
+                case -= 1
+        g += 1
+
+    return [partial_order_groups, partial_order_dataframes]
 
 
 if __name__ == '__main__':
-    test_dataframe()
-    get_partial_order_groups()
+    [partial_order_groups, partial_order_dataframes] = get_partial_order_groups()
+    for i in range(0, len(partial_order_groups)):
+        print('Group ', i + 1)
+        """
+        partial_order_groups[i][j]
+        i = group index
+        j = 0: dataframe with group information
+        j = 1: dataframe with case ID's corresponding to the group
+        """
+        print(partial_order_groups[i][0])
+        print('Corresponding cases')
+        print(partial_order_groups[i][1])
