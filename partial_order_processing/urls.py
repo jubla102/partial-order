4d7d6d1324f746d222ebd82aee577e0b104ab9c1
Julian Blanquez <jubla102@hhu.de>
2021-05-28 18:32:27 +0200

    Get data from backend

--- partial_order_processing/urls.py
+++ partial_order_processing/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from partial_order_processing import partial_order_detection
 from . import views
 
 urlpatterns = [
     path('groups', views.groups, name='groups'),
     path('combinations', views.combinations, name='combinations'),
-    path('delays', views.delays, name='delays')
+    path('delays', views.delays, name='delays'),
+    path('po-groups', partial_order_detection.test_data_structure, name='test')
 ]
--- static/js/partialOrders.js
+++ static/js/partialOrders.js
@@ -1,52 +1,56 @@
-// create svg element:
-var svg = d3.selectAll(".rect").append("svg").attr("width", 800).attr("height", 200)
+axios.get('/partial_order/po-groups')
+    .then((response) => {
+            console.log(response.data)
+            let colorMap = new Map(Object.entries(response.data['colorMap']))
+            let partialOrderGroups = response.data['groups']
 
-hospitalData = [
-    {
-        name: 'Registration',
-        color: '#69b3a2'
-    },
-    {
-        name: 'Leucocytes',
-        color: '#bc5090'
-    },
-    {
-        name: 'CRP',
-        color: '#ff6361'
-    },
-    {
-        name: 'IV Liquid',
-        color: '#ffa600'
-    },
-    {
-        name: 'LacticAcid',
-        color: '#69a3b2'
-    },
-    {
-        name: 'Avinash',
-        color: '#69a3b2'
-    }
-]
+            for (let i = 0; i < partialOrderGroups.length; i++) {
+                drawPartialOrder(partialOrderGroups[i], colorMap)
+            }
+        }
+    );
 
-svg.append('polygon')
-    .attr('points', "50,50 150,50 175,75 150,100 50,100")
-    .attr('fill', '#69b3a2')
-
-svg.append('text')
-    .attr('x', 70)
-    .attr('y', 80)
-    .attr('stroke', 'black')
-    .text("ER Sepsis")
-
-for (let i = 1; i < hospitalData.length; i++) {
+function drawPartialOrder(group, colorMap) {
+    let svg = d3.selectAll("#rect1").append("svg").attr("width", 800).attr("height", 200)
     svg.append('polygon')
-        .attr('points', `${50 + i * 110},50 ${150 + i * 110},50 ${175 + i * 110},75 ${150 + i * 110},100 ${50 + i * 110},100 ${75 + i * 110},75`)
-        .attr('fill', hospitalData[i].color)
+        .attr('points', "50,50 150,50 175,75 150,100 50,100")
+        .attr('fill', colorMap.get(group['cases'][0]['events'][0]['activity']))
 
     svg.append('text')
-        .attr('x', 80 + i * 110)
+        .attr('x', 70)
         .attr('y', 80)
         .attr('stroke', 'black')
-        .text(hospitalData[i].name)
+        .text(group['cases'][0]['events'][0]['activity'])
+
+    for (let j = 1; j < group['cases'][0]['events'].length; j++) {
+        svg.append('polygon')
+            .attr('points', `${50 + j * 110},50 ${150 + j * 110},50 ${175 + j * 110},75 ${150 + j * 110},100 ${50 + j * 110},100 ${75 + j * 110},75`)
+            .attr('fill', colorMap.get(group['cases'][0]['events'][j]['activity']))
+
+        svg.append('text')
+            .attr('x', 80 + j * 110)
+            .attr('y', 80)
+            .attr('stroke', 'black')
+            .text(group['cases'][0]['events'][j]['activity'])
+    }
+}
+
+function getColors() {
+    return [
+        '#EE6352',
+        '#59CD90',
+        '#3FA7D6',
+        '#FAC05E',
+        '#F79D84',
+        '#53A548',
+        '#804E49',
+        '#A69CAC',
+        '#BAF2D8',
+        '#9F2042',
+        '#788585',
+        '#478978',
+        '#FA7921',
+        '#5BC0EB'
+    ]
 }
 
