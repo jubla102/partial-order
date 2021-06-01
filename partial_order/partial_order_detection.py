e7ab0a2895bb74ce8219ec7649b482b480bb5288
Julian Blanquez <jubla102@hhu.de>
2021-06-01 22:43:09 +0200

    Optimize partial order detection algorithm and grouping

--- partial_order/partial_order_detection.py
+++ partial_order/partial_order_detection.py
@@ -1,150 +1,44 @@
 import os
 
-import pandas as pd
 from django.http import JsonResponse
 from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.objects.log.importer.xes import importer
 from pm4py.util.constants import CASE_CONCEPT_NAME
+from pm4py.util.xes_constants import DEFAULT_NAME_KEY
 from pm4py.util.xes_constants import DEFAULT_TIMESTAMP_KEY
 
 from bootstrapdjango import settings
 
 
-def test_data_structure(request):
-    ds = {
-        "colorMap": {
-            "a": "#EE6352",
-            "b": "#59CD90",
-            "c": "#3FA7D6",
-            "d": "#FAC05E",
-            "e": "#F79D84",
-            "f": "#53A548",
-            "g": "#804E49",
-            "h": "#A69CAC",
-            "i": "#BAF2D8",
-            "j": "#9F2042",
-            "k": "#788585",
-            "l": "#478978",
-            "m": "#FA7921",
-            "n": "#5BC0EB"
-        },
-        "groups": [
-            {
-                "groupId": 1,
-                "numerOfCases": 2,
-                "percentage": 0.21,
-                "cases": [
-                    {
-                        "caseId": 1,
-                        "events": [
-                            {
-                                "activity": "c",
-                                "timestamp": "2021-05-11 12:00"
-                            },
-                            {
-                                "activity": "a",
-                                "timestamp": "2021-05-11 12:00"
-                            },
-                            {
-                                "activity": "d",
-                                "timestamp": "2021-05-11 12:00"
-                            }
-                        ]
-                    },
-                    {
-                        "caseId": 2,
-                        "events": [
-                            {
-                                "activity": "c",
-                                "timestamp": "2021-05-11 13:00"
-                            },
-                            {
-                                "activity": "a",
-                                "timestamp": "2021-05-11 13:00"
-                            },
-                            {
-                                "activity": "d",
-                                "timestamp": "2021-05-11 13:00"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                "groupId": 2,
-                "numerOfCases": 1,
-                "percentage": 0.1,
-                "cases": [
-                    {
-                        "caseId": 1,
-                        "events": [
-                            {
-                                "activity": "a",
-                                "timestamp": "2021-05-11 12:00"
-                            },
-                            {
-                                "activity": "b",
-                                "timestamp": "2021-05-11 12:01"
-                            },
-                            {
-                                "activity": "c",
-                                "timestamp": "2021-05-11 12:01"
-                            },
-                            {
-                                "activity": "d",
-                                "timestamp": "2021-05-11 12:02"
-                            },
-                            {
-                                "activity": "e",
-                                "timestamp": "2021-05-11 12:02"
-                            },
-                            {
-                                "activity": "f",
-                                "timestamp": "2021-05-11 12:03"
-                            },
-                            {
-                                "activity": "g",
-                                "timestamp": "2021-05-11 12:04"
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
-    return JsonResponse(ds, safe=False)
-
-
-def get_partial_orders_from_selected_file():
-    """
-    simple-test.xes event log contains 9 cases.
-    The following case ids are partial orders: 1, 4, 6, 7, 8, 9
-    The groups are: (1, 7, 8), (4, 6), (9)
-
-    TODO Replace hard coded file by user selection
-    """
+def get_partial_orders_from_selected_file(request):
     event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
-    absolute_file_path = os.path.join(event_logs_path, 'simple-test.xes')
+    absolute_file_path = os.path.join(event_logs_path, 'Sepsi_Cases-Event_Log.xes')
     event_log = importer.apply(absolute_file_path)
-
-    return get_partial_orders_from_event_log(event_log)
-
-
-def get_partial_orders_from_event_log(event_log):
     df = log_converter.apply(event_log, variant=log_converter.Variants.TO_DATA_FRAME)
-    case_identifiers = df[CASE_CONCEPT_NAME].unique()
-    partial_orders = pd.DataFrame()
-    for case_id in case_identifiers:
-        case = df.loc[df[CASE_CONCEPT_NAME] == case_id]
-        if is_partial_order(case):
-            partial_orders = partial_orders.append(case)
+    df = df[[CASE_CONCEPT_NAME, DEFAULT_NAME_KEY, DEFAULT_TIMESTAMP_KEY]]
+    df = df.sort_values(by=['time:timestamp', 'concept:name'])
+    partial_order_groups = {'totalNumberOfTraces': len(event_log), 'groups': {}}
+    df.groupby(CASE_CONCEPT_NAME).apply(lambda x: check_for_partial_order(x, partial_order_groups))
+    df[DEFAULT_TIMESTAMP_KEY] = df[DEFAULT_TIMESTAMP_KEY].astype(str)
+    df = df.sort_values(by=[DEFAULT_TIMESTAMP_KEY, DEFAULT_NAME_KEY])
 
-    return log_converter.apply(partial_orders, variant=log_converter.Variants.TO_EVENT_LOG)
+    df.groupby(CASE_CONCEPT_NAME).apply(lambda x: check_for_partial_order(x, partial_order_groups))
 
-
-def is_partial_order(case):
-    return not case[DEFAULT_TIMESTAMP_KEY].is_unique
+    return JsonResponse(partial_order_groups, safe=False)
 
 
-if __name__ == '__main__':
-    print(get_partial_orders_from_selected_file())
+def check_for_partial_order(case, partial_order_groups):
+    events = []
+    if not case[DEFAULT_TIMESTAMP_KEY].is_unique:
+        case.groupby(DEFAULT_TIMESTAMP_KEY).apply(lambda x: append_partial_ordered_events(x, events))
+        key = ''.join(events)
+
+        if key in partial_order_groups:
+            partial_order_groups['groups'][key]['numberOfCases'] = partial_order_groups[key]['numberOfCases'] + 1
+        else:
+            partial_order_groups['groups'][key] = {'numberOfCases': 1}
+            partial_order_groups['groups'][key]['cases'] = [[*case.to_dict('index').values()]]
+
+
+def append_partial_ordered_events(x, events):
+    events.extend(['|'] + x[DEFAULT_NAME_KEY].values.tolist() + ['|'])
--- partial_order/urls.py
+++ partial_order/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from partial_order import partial_order_detection
-from . import views
+from partial_order import views
 
 urlpatterns = [
     path('groups', views.groups, name='groups'),
     path('combinations', views.combinations, name='combinations'),
     path('delays', views.delays, name='delays'),
-    path('po-groups', partial_order_detection.test_data_structure, name='test')
+    path('po-groups', partial_order_detection.get_partial_orders_from_selected_file, name='po-groups')
 ]
