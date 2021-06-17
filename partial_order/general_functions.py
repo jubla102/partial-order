898c3212f175790202a79635957f3fa96937f23c
Julian Blanquez <jubla102@hhu.de>
2021-06-17 22:40:31 +0200

    Fix merge conflicts

--- partial_order/general_functions.py
+++ partial_order/general_functions.py
@@ -1,24 +1,44 @@
+import json
 import os
 
-from pm4py.objects.conversion.log import converter as log_converter
-from pm4py.objects.log.importer.xes import importer
-
-from bootstrapdjango import settings
+import seaborn as sns
+from django.conf import settings
 
 
-def max_trace_length():
-    """
-        Returns
-        -------
-        result: maximum length of a trace in an event log
-    """
+def get_selected_file_path():
     event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
-    absolute_file_path = os.path.join(event_logs_path, 'simple-test.xes')
-    event_log = importer.apply(absolute_file_path)
-    df = log_converter.apply(event_log, variant=log_converter.Variants.TO_DATA_FRAME)
-    val = df.groupby(["case:concept:name"]).count().max()
-    return val["concept:name"]
+    return os.path.join(event_logs_path, settings.EVENT_LOG_NAME)
 
 
-if __name__ == '__main__':
-    print(max_trace_length())
+def get_groups_file_path():
+    event_logs_path = os.path.join(settings.MEDIA_ROOT, "temp")
+    file_name = 'groups_' + os.path.splitext(settings.EVENT_LOG_NAME)[0] + '.json'
+    return os.path.join(event_logs_path, file_name)
+
+
+def get_longest_activity_name(activities):
+    longest_activity_name = ''
+    for activity in activities:
+        if len(longest_activity_name) < len(activity):
+            longest_activity_name = activity
+
+    return longest_activity_name
+
+
+def get_meta_data():
+    return {'colors': settings.COLORS,
+            'longestActivityName': settings.LONGEST_ACTIVITY_NAME,
+            'textWidths': settings.TEXT_WIDTHS}
+
+
+def get_colors(activities):
+    color_palette = sns.color_palette(None, len(activities)).as_hex()
+    colors = {}
+    for i, activity in enumerate(activities):
+        colors[activity] = color_palette[i]
+
+    return colors
+
+
+def get_form_data(request, key):
+    return json.loads(request.POST.dict()[key])
